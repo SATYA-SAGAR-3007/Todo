c8f1431b457592bc46a4797a947deedb1c0dfe17
SATYA_SAGAR <sssagarrao3007@gmail.com>
2025-09-24 22:39:20 +0530

    This is a Commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from "react";
 import "./index.css";
 class About extends Component {
@@ -13,19 +12,3 @@ class About extends Component {
 }
 
 export default About;
-=======
-import { Component } from "react";
-import "./index.css";
-class About extends Component {
-  render() {
-    return (
-      <div className="about">
-        <h1>About</h1>
-        <p>This is the About Page</p>
-      </div>
-    );
-  }
-}
-
-export default About;
->>>>>>> c6d68d36fa4cbec1158409ba63957665446eff27
